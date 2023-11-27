16dab52599bdb2aaecd6f2d85a427ab413e1bdfe
noviceGuru <amin.arabi@gmail.com>
2023-11-27 22:54:27 +0100

    timer works with the go and pause button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import Clock from '@/components/atoms/clock/Clock'
+import { useState } from "react"
+
+import Clock from "@/components/atoms/clock/Clock"
+import Button from "@/components/atoms/button/button"
 
 function App() {
+    const [isRunning, setIsRunning] = useState<boolean>(false)
+
     return (
         <div className="bg-red-400">
-            <Clock />
+            <Button
+                className="border-2 hover:bg-yellow-200 focus:bg-red-500 active:bg-neutral-500 p-2 rounded-md"
+                onClick={() => setIsRunning(!isRunning)}
+            >
+                Stop/Go
+            </Button>
+            <Clock isRunning={isRunning} />
         </div>
     )
 }
--- src/components/atoms/clock/Clock.tsx
+++ src/components/atoms/clock/Clock.tsx
@@ -1,3 +1,27 @@
-export default function Clock() {
-    return <p>Time</p>
+import { useEffect, useState } from "react"
+
+export default function Clock({ isRunning }: { isRunning: boolean }) {
+    const [seconds, setSeconds] = useState<number>(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (isRunning) {
+                setSeconds(prevSeconds => prevSeconds + 1)
+            }
+        }, 1000)
+
+        return () => clearInterval(interval)
+    }, [isRunning])
+
+    const clockMinutes = Math.floor(seconds / 60)
+    const clockSeconds = Math.floor(seconds % 60)
+
+    return (
+        <div>
+            <p>
+                {clockMinutes < 10 ? 0 : ""} {clockMinutes} :
+                {clockSeconds < 10 ? 0 : ""} {clockSeconds}
+            </p>
+        </div>
+    )
 }
