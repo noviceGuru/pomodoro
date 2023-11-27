b4e7981eade15052767efae2db6adf4874fb3670
noviceGuru <amin.arabi@gmail.com>
2023-11-27 21:13:48 +0100

    absolute path was added
    title changed to Pomodoro Recorder from boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Clock from '@/components/atoms/clock/Clock'
+
 function App() {
-    return <div className="bg-red-400">Pomodoro App</div>
+    return (
+        <div className="bg-red-400">
+            <Clock />
+        </div>
+    )
 }
 
 export default App
