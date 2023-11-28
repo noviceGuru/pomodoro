fd5eae7ad2f0c42e7fa7873d2022f8a69f8e6a16
noviceGuru <amin.arabi@gmail.com>
2023-11-28 08:08:21 +0100

    absolute imports changed from @ to no sign
    a wrapper container made for all the components and used in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { useState } from "react"
 
-import Clock from "@/components/atoms/clock/Clock"
-import Button from "@/components/atoms/button/button"
+import Clock from "components/atoms/clock/Clock"
+import Button from "components/atoms/button/button"
+import Container from "components/atoms/container/container"
 
 function App() {
     const [isRunning, setIsRunning] = useState<boolean>(false)
 
     return (
-        <div className="bg-red-400">
+        <Container>
             <Button
                 className="border-2 hover:bg-yellow-200 focus:bg-red-500 active:bg-neutral-500 p-2 rounded-md"
                 onClick={() => setIsRunning(!isRunning)}
             >
-                Stop/Go
+                {isRunning ? `Stop` : `Go`}
             </Button>
             <Clock isRunning={isRunning} />
-        </div>
+        </Container>
     )
 }
 
