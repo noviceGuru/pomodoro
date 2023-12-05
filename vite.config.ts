346dc297e1c544fe8aeed175980e352b7c9020b2
noviceGuru <amin.arabi@gmail.com>
2023-12-05 04:52:41 +0100

    react testing library added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import path from "path"
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
     plugins: [react()],
-
+    test: { globals: true, environment: "jsdom" },
     resolve: {
         alias: {
             "components": path.resolve(__dirname, "src/components"),
