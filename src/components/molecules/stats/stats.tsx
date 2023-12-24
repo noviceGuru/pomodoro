22f61a3099986e25ce094b0a41c1ffe19ba92bc5
noviceGuru <amin.arabi@gmail.com>
2023-12-24 12:00:05 +0100

    logic separated in a hook, at stats

--- src/components/molecules/stats/stats.tsx
+++ src/components/molecules/stats/stats.tsx
@@ -1,31 +1,14 @@
-import { useState, useEffect } from "react"
-
 import StatsHeader from "components/atoms/stats-header/stats-header"
 import StatsTable from "components/atoms/stats-table/stats-table"
 
-import { getAllLaps, deleteAllLaps, deleteOneLap, Lap } from "features/db"
+import useStats from "components/molecules/stats/useStats"
 
 export default function Stats({
     setShow,
 }: {
     setShow: React.Dispatch<React.SetStateAction<boolean>>
 }) {
-    const [tableData, setTableData] = useState<Lap[]>([])
-
-    useEffect(() => {
-        //@ts-ignore
-        getAllLaps().then((e: Lap) => setTableData(e))
-    }, [])
-
-    const handleDeleteAll = () => {
-        //@ts-ignore
-        deleteAllLaps().then(res => getAllLaps().then((e: Lap) => setTableData(e)))
-    }
-
-    const handleDeleteOne = (id: string) => {
-        //@ts-ignore
-        deleteOneLap(id).then(res => getAllLaps().then((e: Lap) => setTableData(e)))
-    }
+    const { tableData, handleDeleteAll, handleDeleteOne } = useStats()
 
     return (
         <div className="bg-slate-400 h-4/5 w-4/6 rounded-2xl p-10 opacity-100 overflow-y-scroll no-scrollbar">
---
+++ src/components/molecules/stats/useStats.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react"
+import { getAllLaps } from "features/db"
+import { Lap } from "features/db"
+
+export default function useStats() {
+    const [tableData, setTableData] = useState<Lap[]>([])
+
+    useEffect(() => {
+        //@ts-ignore
+        getAllLaps().then((e: Lap) => setTableData(e))
+    }, [])
+
+    const handleDeleteAll = () => {
+        //@ts-ignore
+        deleteAllLaps().then(res => getAllLaps().then((e: Lap) => setTableData(e)))
+    }
+
+    const handleDeleteOne = (id: string) => {
+        //@ts-ignore
+        deleteOneLap(id).then(res => getAllLaps().then((e: Lap) => setTableData(e)))
+    }
+
+    return { tableData, handleDeleteAll, handleDeleteOne }
+}
